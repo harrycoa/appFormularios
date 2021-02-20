964a918adbff380c01954cd8cecced53f9951aeb
Harry Coa <rodol_2000@hotmail.com>
2021-02-19 21:11:22 -0500

    ngmodel para mostrar en consola

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -10,6 +11,10 @@ export class TemplateComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar (forma: NgForm) {
+    console.log(forma.value);
   }
 
 }
