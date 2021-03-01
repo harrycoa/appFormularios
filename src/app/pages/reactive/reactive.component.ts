6ecf0f42f35a80249345e9af297e6eeb61966bee
Harry Coa <rodol_2000@hotmail.com>
2021-02-28 19:11:14 -0500

    agregar valores a un array ya establecido

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -13,22 +13,99 @@ export class ReactiveComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.crearFormulario();
+    this.cargarDataAlFormulario();
    }
 
   ngOnInit(): void {
   }
 
+  get pasatiempos(){
+    return this.forma.get('pasatiempos') as FormArray;
+  }
+
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched
+  }
+
+  get apellidoNoValido() {
+    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched
+  }
+
+  get correoNoValido() {
+    return this.forma.get('correo').invalid && this.forma.get('correo').touched
+  }
+
+  get distritoNoValido() {
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched
+  }
+
+
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre  : ['', [Validators.required, Validators.minLength(5)] ],
-      apellido: [''],
-      correo  : ['',[Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required]]
+      apellido: ['', [Validators.required, Validators.minLength(3)] ],
+      correo  : ['',[Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'), Validators.required]],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required ],
+      }),
+      pasatiempos: this.fb.array([
+        //[],[],[],[],[]
+      ])
     });
 
   }
 
-  guardar() {
-    console.log( this.forma );
+  cargarDataAlFormulario(){
+    // set value para todos los campos
+    // reset para unos cuantos campos
+    this.forma.reset({
+      nombre: 'harry',
+      //apellido: 'coo',
+      correo: 'hco@co.co',
+      direccion: {
+        distrito: 'coo',
+        ciudad: 'coo'
+      }
+    });
+
+    // una manera de cargar al formulario un arreglo ya preestablecido
+    ['jugar','dormir'].forEach(valor => this.pasatiempos.push( this.fb.control(valor)))
   }
+
+  agregarPasatiempo(){
+    this.pasatiempos.push( this.fb.control('', Validators.required));
+  }
+  borrarPasatiempo(i: number){
+    this.pasatiempos.removeAt(i);
+  }
+
+  guardar() {
+
+    console.log( this.forma );
+
+    if ( this.forma.invalid ) {
+
+      return Object.values( this.forma.controls ).forEach( control => {
+
+        if ( control instanceof FormGroup ) {
+            Object.values( control.controls ).forEach( control =>  control.markAsTouched() );
+        } else {
+            control.markAsTouched();
+        }
+
+    });
+
+    }
+    // resetear los campos
+    this.forma.reset();
+  }
+
+
 
 }
